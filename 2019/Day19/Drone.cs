c7241c9523d12c7bcedf5b07f488f9833d6080b2
Mark Hurenkamp <mark.hurenkamp@signify.com>
2019-12-19 15:31:10 +0100

    Solution for Day19

---
+++ 2019/Day19/Drone.cs
@@ -0,0 +1,106 @@
+﻿using AoC2019.Intcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AoC2019.Day19
+{
+    public class Position
+    {
+        public int X;
+        public int Y;
+    }
+
+    public class Area
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    public class Drone
+    {
+        private Queue<BigInteger> _inputqueue = new Queue<BigInteger>();
+        private Queue<BigInteger> _outputqueue = new Queue<BigInteger>();
+        private Semaphore _guardinput = new Semaphore(0, 2);
+        private Semaphore _guardoutput = new Semaphore(0, 1);
+        private List<BigInteger> _inputs = new List<BigInteger>() { 1, 4, 2, 3 };
+
+        public Drone()
+        {
+        }
+
+        public void Halt()
+        {
+            _inputqueue.Enqueue(0);
+            _guardinput.Release();
+        }
+
+        public List<Position> ProbeArea(Area area)
+        {
+            List<Position> map = new List<Position>();
+            for (int x = area.Left; x <= area.Right; x++)
+                for (int y = area.Top; y <= area.Bottom; y++)
+                {
+                    var pos = new Position() { X = x, Y = y };
+                    if (Probe(pos) > 0) map.Add(pos);
+                }
+            return map;
+        }
+
+        public List<Position> ProbeCorners(Area area)
+        {
+            //Console.WriteLine("ProbeCorners({0},{1},{2},{3}))", area.Left,area.Top,area.Right,area.Bottom);
+            List<Position> map = new List<Position>();
+            foreach(int x in new int[] { area.Left, area.Right } )
+                foreach (int y in new int[] { area.Top, area.Bottom })
+                {
+                    var pos = new Position() { X = x, Y = y };
+                    if (Probe(pos) > 0) map.Add(pos);
+                }
+            return map;
+        }
+
+        public int Probe(Position pos)
+        {
+            var computer = new Computer();
+            computer.LoadProgram(Input.Data);
+            computer.input = InputProvider;
+            computer.output = OutputHandler;
+            Thread t = new Thread(new ThreadStart(computer.Run));
+            t.Start();
+
+            //Console.WriteLine("Probe({0},{1}): Enqueuing ... ", pos.X, pos.Y);
+            _inputqueue.Enqueue(pos.X);
+            _inputqueue.Enqueue(pos.Y);
+            _guardinput.Release(2);
+            //Console.WriteLine("Probe({0},{1}): Waiting ... ", pos.X, pos.Y);
+            _guardoutput.WaitOne();
+            int result = (int)_outputqueue.Dequeue();
+            //Console.WriteLine("Probe({0},{1}): Return {2}.", pos.X, pos.Y, result);
+            return result;
+        }
+
+        private BigInteger InputProvider()
+        {
+            //Console.WriteLine("InputProvider(): Waiting ... ");
+            _guardinput.WaitOne();
+            var v = _inputqueue.Dequeue();
+            //Console.WriteLine("InputProvider(): Return {0}.",v);
+            return v;
+        }
+
+        private void OutputHandler(BigInteger value)
+        {
+            //Console.WriteLine("OutputHandler({0}): Enqueing ... ", value);
+            _outputqueue.Enqueue(value);
+            _guardoutput.Release();
+            //Console.WriteLine("OutputHandler({0}): Released.", value);
+        }
+    }
+}
---
+++ 2019/Day19/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace AoC2019.Day19
+{
+    public class Main : Base
+    {
+        protected override void Part1()
+        {
+            var drone = new Drone();
+            var map = drone.ProbeArea(new Area() { Left = 0, Top = 0, Right = 49, Bottom = 49 });
+            drone.Halt();
+
+            var area = GetArea(map);
+
+            Console.WriteLine("Result: {0}", map.Count);
+        }
+
+        public Area GetArea(List<Position> map)
+        {
+            if (map.Count < 1) return null;
+
+            var result = new Area() { Left = map[0].X, Right = map[0].X, Top = map[0].Y, Bottom = map[0].Y };
+            foreach (var pos in map)
+            {
+                result.Left =   (pos.X < result.Left) ?   pos.X : result.Left;
+                result.Right =  (pos.X > result.Right) ?  pos.X : result.Right;
+                result.Top =    (pos.Y < result.Top) ?    pos.Y : result.Top;
+                result.Bottom = (pos.Y > result.Bottom) ? pos.Y : result.Bottom;
+            }
+            return result;
+        }
+
+        protected override void Part2()
+        {
+            var drone = new Drone();
+            bool halt = false;
+            var x = 0;
+            var y = -1;
+
+            while (!halt)
+            {
+                // x and y are following the bottomleft corner of the beam
+                y += 1;
+                while (drone.Probe(new Position { X=x, Y=y+99 }) <1) { x++; };
+
+                // determine if ship fits
+                var area = new Area() { Left = x, Top = y, Right = x + 99, Bottom = y+99 };
+                if (drone.ProbeCorners(area).Count >3) halt = true;
+            }
+
+            Console.WriteLine("Result:    {0}", x*10000+y);
+        }
+    }
+}
