36b9dccaca5edf7ee231f30c616ab8aac7d2aa38
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2025-12-14 02:16:54 +0100

    day-6 part 2 solution

--- 2025/day-6/src/main.rs
+++ 2025/day-6/src/main.rs
@@ -9,50 +9,100 @@ fn read_lines(filename: &str) -> Vec<String> {
     content
 }
 
-fn parse_input_1(input: &Vec<String>) -> Vec<(char,Vec<i64>)> {
+fn combine_actions_and_valuse(actions: Vec<char>, values: Vec<Vec<i64>>) -> Vec<(char,Vec<i64>)> {
     let mut result = vec![];
-
-    let mut lines = input[..input.len()-1].iter();
-    let mut rows = vec![];
-    while let Some(line) = lines.next() {
-        rows.push(line.split_ascii_whitespace()
-            .map(|value| value.parse::<i64>().unwrap())
-            .collect::<Vec<_>>());
+    for (i,column) in values.into_iter().enumerate() {
+        let action = actions.get(i).unwrap();
+        result.push((*action,column));
     }
-    let values: Array2D<i64> = Array2D::from_rows(&rows).unwrap();
-    let columns = values.columns_iter();
+    result
+}
 
-    let actions = input[input.len()-1].split_ascii_whitespace()
+fn get_actions(input: &Vec<String>) -> Vec<char> {
+    input[input.len()-1].split_ascii_whitespace()
         .map(|value| match value {
             "+" => '+',
             "*" => '*',
             _ => panic!(),
         })
-        .collect::<Vec<_>>();
-
-    for (i,column) in columns.enumerate() {
-        let action = actions.get(i).unwrap();
-        result.push((*action,column.map(|&value|value).collect::<Vec<_>>()));
-    }
-
-    result
+        .collect::<Vec<_>>()
 }
 
-fn parse_input_2(input: &Vec<String>) -> Vec<(char,Vec<i64>)> {
-    let result = vec![];
+fn get_values_1(input: &Vec<String>) -> Vec<Vec<i64>> {
+    let mut lines = input[..input.len()-1].iter();
+    let mut rows = vec![];
+
+    // values are per row listed horizontally separated by a space
+    while let Some(line) = lines.next() {
+        rows.push(line.split_ascii_whitespace()
+            .map(|value| value.parse::<i64>().unwrap())
+            .collect::<Vec<_>>());
+    }
+
+    // values are collected in a Array2D where
+    // each column in the array is input for a math
+    // operation.
+    let values: Array2D<i64> = Array2D::from_rows(&rows).unwrap();
+
+    // we iterate over the columns, to convert the
+    // array into a Vec<Vec<i64>> where the inner
+    // vec's each contain the values for a single problem
+    values.columns_iter().map(|column|
+        column.map(|&value|value).collect::<Vec<i64>>()
+    ).collect::<Vec<_>>()
+}
+
+fn get_values_2(input: &Vec<String>) -> Vec<Vec<i64>> {
+    let lines = input[..input.len()-1].iter();
+    let rows = lines.map(|row| row.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let chars = Array2D::from_rows(&rows).unwrap();
+
+    // values are listed vertically, where values for
+    // different problems are separated by a column
+    // of spaces
+    let mut result = vec![];
+    let mut record = vec![];
+    let mut columns = chars.columns_iter();
+    while let Some(column) = columns.next() {
+
+        // convert the vertical column to a string, so we can attempt to parse it
+        let column: String = column.collect::<Vec<_>>().iter().map(|&c|*c).collect();
+
+        // remove trailing whitespace
+        if let Some(s) = column.split_ascii_whitespace().next() {
+
+            // and parse the result
+            if let Ok(value) = s.parse::<i64>() {
+                record.push(value);
+            } else { panic!(); }
+        } else {
+
+            // nothing remains after stripping whitespace, so we must
+            // have hit an empty column.
+            // let's push the record to result and start a new record
+            result.push(record);
+            record = vec![];
+        }
+    }
+
+    // no more columns to process, but we still need to
+    // push the final record to result.
+    result.push(record);
+
     result
 }
 
 fn calculate_total(records: Vec<(char, Vec<i64>)>) -> i64 {
     let mut total = 0;
 
-    let mut iter = records.into_iter();
+    let mut iter = records.clone().into_iter();
     while let Some((action,values)) = iter.next() {
-        total += match action {
+        let result = match action {
             '+' => values.into_iter().sum::<i64>(),
             '*' => values.into_iter().product::<i64>(),
             _ => panic!()
-        }
+        };
+        total += result;
     }
 
     total
@@ -60,8 +110,10 @@ fn calculate_total(records: Vec<(char, Vec<i64>)>) -> i64 {
 
 fn main() {
     let input = read_lines("./input.txt");
-    let parsed_input = parse_input_1(&input);
-    println!("solution 1: {}",calculate_total(parsed_input)); // 6172481852142
-    let parsed_input = parse_input_2(&input);
-    println!("solution 2: {}",calculate_total(parsed_input)); //
+
+    let records = combine_actions_and_valuse(get_actions(&input),get_values_1(&input));
+    println!("solution 1: {}",calculate_total(records)); // 6172481852142
+
+    let records = combine_actions_and_valuse(get_actions(&input),get_values_2(&input));
+    println!("solution 2: {}",calculate_total(records)); // 10188206723429
 }
